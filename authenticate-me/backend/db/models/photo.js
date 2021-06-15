0d4df9dfb3b975c91210868e6e11e8c6ced1674a
Malik Moss <malikmoss33@gmail.com>
2021-06-15 10:52:56 -0500

    seed data for photos

--- authenticate-me/backend/db/models/photo.js
+++ authenticate-me/backend/db/models/photo.js
@@ -5,8 +5,6 @@ module.exports = (sequelize, DataTypes) => {
     authorId: DataTypes.INTEGER
   }, {});
   Photo.associate = function(models) {
-    // associations can be defined here
-    // Photo.belongsTo(models.Album, {foreignKey: 'albumId'});
     Photo.belongsTo(models.User, {foreignKey: 'authorId'});
     Photo.belongsToMany(models.Tag, {foreignKey: 'tagId', through: 'photo_tags', otherKey: 'photoId'});
   };
--- authenticate-me/backend/db/seeders/20210224183641-Photos.js
+++ authenticate-me/backend/db/seeders/20210224183641-Photos.js
@@ -2,6 +2,92 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert('Photos', [
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/taupo_newzealand/airplane_active1.jfif',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/st.+petersburg%2C+russia/active.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/solarpowered_tag/active2.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/solarpowered_tag/active1.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/oct+harbour_shenzhen_china/deceased1.JPEG',
+        authorId: 1,
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/nonactive.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/monterey_ca/active.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/mcthriller/mcthriller2.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/lisbonzoo_lisbon_portugal/active.PNG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/lisbonzoo_lisbon_portugal/active3.PNG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/best_netherlands/partially_active1.png',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/best_netherlands/partially_active2.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcpiccs/best_netherlands/partially_active3.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/antigua.jfif',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/harber-sydney.jfif',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/lapres.jfif',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/mcdiner.png',
+        authorId: 1,
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/nonactive.JPEG',
+        authorId: 1
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/singlearc.jfif',
+        authorId: 1,
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/sydney_australia2.jfif',
+        authorId: 1,
+      },
+      {
+        photoURL: 'https://mcflicc-photos.s3.amazonaws.com/sydney_australia3.jfif',
+        authorId: 1
+      }
+    ])
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
